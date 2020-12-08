2c3a0a4829dd2a1c5ccadefb6ad6fac081129b11
Nikki <piknik@me.com>
2020-12-08 08:09:01 -0700

    finished dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,22 @@
+class Dragon {
+  constructor(name, rider, hungry = true) {
+    this.name = name
+    this.rider = rider
+    this.hungry = hungry
+    this.eating = 0;
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.eating++
+    if (this.eating === 3) {
+      this.hungry = false
+      this.eating = 0
+    }
+  }
+}
+
+module.exports = Dragon
