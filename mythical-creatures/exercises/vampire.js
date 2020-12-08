c67f17ebf484e0cc638e301fd7143f87eb3a7b37
Nikki <piknik@me.com>
2020-12-08 08:21:17 -0700

    vampires done

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,6 +1,18 @@
 class Vampire {
-  constructor() {
+  constructor(name, pet = 'bat', thirsty = true, ouncesDrank = 0) {
+    this.name = name
+    this.pet = pet
+    this.thirsty = thirsty
+    this.ouncesDrank = ouncesDrank
+  }
 
+  drink() {
+    if (this.ouncesDrank === 50) {
+      return "I'm too full to drink anymore!"
+    } else {
+      this.ouncesDrank += 10
+      this.thirsty = false
+    }
   }
 }
 
